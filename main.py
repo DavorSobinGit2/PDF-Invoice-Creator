a9f84b16c8a04b35701843b0f3f7ad072867d8b1
Davor Sobin <davor.sobin@gmail.com>
2024-03-18 01:17:40 -0500

    Third Commit Still working on it...

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
+import time
 
 filepaths = glob.glob("invoices/*.xlsx")
 
@@ -14,4 +15,7 @@ for index, filepath in enumerate(filepaths):
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice Nr.{invoice_number}")
+    pdf.ln(0)
+    curr_date = time.strftime("%Y.%m.%d")
+    pdf.cell(w=50, h=30, txt=f"Date: {curr_date}")
     pdf.output(f"PDFs/Invoice-{index+1}.pdf")
